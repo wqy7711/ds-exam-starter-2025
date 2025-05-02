e4a03ae2d347d9c684512fd48ea82aa39ca91a95
unknown <944325009@qq.com>
2025-05-02 10:34:13 +0100

    EDA architecture.

--- lib/exam-stack.ts
+++ lib/exam-stack.ts
@@ -123,6 +123,13 @@ export class ExamStack extends cdk.Stack {
         REGION: "eu-west-1",
       },
     });
+
+    topic1.addSubscription(new subs.SqsSubscription(queueA));
+    topic1.addSubscription(new subs.LambdaSubscription(lambdaYFn));
+    lambdaXFn.addEventSource(new events.SqsEventSource(queueA, {
+      batchSize: 10
+    }));
+
     
   }
 }
